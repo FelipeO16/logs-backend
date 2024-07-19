1179e69693beea4a90ffc5d993aa2894bacf4b78
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-07-19 16:17:13 -0300

    token

--- validators/auth.ts
+++ validators/auth.ts
@@ -5,5 +5,6 @@ export const registerValidator = vine.compile(
     full_name: vine.string().maxLength(100),
     email: vine.string().email().normalizeEmail(),
     password: vine.string().minLength(8),
+    token: vine.string().maxLength(100),
   })
 )
